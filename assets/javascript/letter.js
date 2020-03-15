6bc1acec86b6493e041d5a09e23decbe16a05b38
Justin Acosta <55920861+leopoldleopold@users.noreply.github.com>
2020-03-15 13:11:38 -0400

    Update letter.js

--- assets/javascript/letter.js
+++ assets/javascript/letter.js
@@ -1,46 +1,50 @@
-//Dictionary
-
-var dictionary = ["a", "b", "c", "d", "e", "f", "g", 
-"h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-//Randomized Word Selection
-
-var letter = dictionary[Math.floor(Math.random() * dictionary.length)];
-
-//Variables to determine 
+// variables
+var dictionary = ["a", "b", "c", "d", "e", "f", "g",
+    "h", "i", "j", "k", "l", "m", "n", "o", "p",
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//constant variables
 var correct = 0;
 var incorrect = 7;
-
-//Variable Display
-document.getElementById("incorrect").innerHTML = incorrect;
-document.getElementById("correct").innerHTML = correct;
-document.getElementById("user").innerHTML = guess;
-
-//UserInPut
-
-//BeginGame 
-function beginGame() {
-    var letter = dictionary[Math.floor(Math.random() * dictionary.length)];
-    document.onkeyup = function(event) {
-        var guess = event.key;
-        if (guess === letter) {
+var guesses = [];
+// game function
+function initGame() {
+    document.getElementById("nar").innerHTML = "AND GUESS!";
+    // random letter 
+    var rndm = dictionary[Math.floor(Math.random() * dictionary.length)];
+    document.onkeyup = function (event) {
+        var guess = event.key.toLowerCase();
+        // guess is correct
+        if (guess === rndm) {
             correct++;
-            incorrect = 7;
-            guess = [];
-            document.getElementById("nar").innerHTML = "Tim is pleased by your ESP abilties.";
-        }  else {
-            document.getElementById("incorrect").innerHTML = incorrect - 1;
-             
+            document.getElementById("nar").innerHTML = "Tim is pleased by your " +
+                "esp abilities!";
+            reset();
         }
-        
-        if (incorrect == 0) {
-            document.getElementById("nar").innerHTML = "Tim is displeased by your failure and grants you additional tries.";
-            incorrect = 7;
-            guess = [];
-            document.getElementById("user").innerHTML = guess;
+        // if user runs out of guesses
+        else if (incorrect === 0) {
+            document.getElementById("nar").innerHTML = "Tim is displeased by " +
+                "your failure and grants you additional tries!";
+            reset();
         }
+        // if guess is incorrect
+        else if (guess != rndm) {
+            // if user guess has already been attempted
+            if (dictionary.indexOf(guess)) {
+                document.getElementById("nar").innerHTML = "Tim's ESP abilities suggest" +
+                    " that '" + guess + "' has already been guessed. Try Again!";
+            }
+            incorrect--;
+            guesses.push(guess);
+        }
+        document.getElementById("user").innerHTML = guess;
+        document.getElementById("guesses").innerHTML = guesses;
+        document.getElementById("correct").innerHTML = correct;
+        document.getElementById("incorrect").innerHTML = incorrect;
     }
-}
-
-
+};
+// function to reset user tries and empty guess array
+function reset() {
+    incorrect = 7;
+    guesses = [];
+    initGame();
+};
